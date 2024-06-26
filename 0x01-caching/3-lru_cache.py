0d8452bd44f80a7da63104dde007653a718a774b
Shaker Sharabi <sheko100@icloud.com>
2024-06-26 23:45:03 +0300

    Add 3-lru_cache.py file

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,9 +10,7 @@ class LRUCache(BaseCache):
     def __init__(self):
         """Initialzes the instance"""
         super().__init__()
-        self.keys_index = {}
-        self.index = 0
-        self.lru_index = 0
+        self.queue = []
 
     def put(self, key, item):
         """Adds an item to the cache and replaces the least recently used item
@@ -25,6 +23,7 @@ class LRUCache(BaseCache):
         if key is None or item is None:
             return
 
+        """
         if (key not in self.keys_index.values() and
                 len(self.cache_data) == super().MAX_ITEMS):
             lru_key = self.keys_index[self.lru_index]
@@ -36,9 +35,19 @@ class LRUCache(BaseCache):
         elif key in self.keys_index.values():
             del_index = [i for i, k in self.keys_index.items() if k == key][0]
             del self.keys_index[del_index]
+        """
+        if len(self.cache_data) == super().MAX_ITEMS:
+            first_key = self.queue[0]
 
-        self.keys_index[self.index] = key
-        self.index += 1
+            if key in self.queue:
+                self.queue.remove(key)
+            else:
+                del self.cache_data[first_key]
+                self.queue = self.queue[1:]
+                print('DISCARD: {}'.format(first_key))
+
+        self.queue.append(key)
+        print(self.queue)
         self.cache_data[key] = item
 
     def get(self, key):
@@ -51,6 +60,8 @@ class LRUCache(BaseCache):
         value = None
 
         if key and key in cache:
+            self.queue.remove(key)
+            self.queue.append(key)
             value = cache[key]
 
         return value
