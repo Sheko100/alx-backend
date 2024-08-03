3352af8d75e8a2bc34ee312116be0df8e6bf07be
Shaker Sharabi <shakersharabi100@gmail.com>
2024-08-04 01:24:56 +0300

    Add root route

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that defines a flask app with root route
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    """prints Hello World to the root route"""
+    return render_template('0-index.html')
